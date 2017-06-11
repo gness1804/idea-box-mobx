f3a882b085209cf5351a522f74b918a2a2d9ea25
gness1804 <graham.nessler@gmail.com>
2017-06-11 17:21:56 -0500

    Idea names are finally rendering on page in list

--- src/Main.js
+++ src/Main.js
@@ -1,47 +1,75 @@
-import React, { Component } from 'react';
-// import { autorun } from 'mobx';
+import React from 'react';
+import { observer } from 'mobx-react';
 import store from './store';
 import Idea from './Idea';
 
-class Main extends Component {
-
-  addIdea = () => {
+const Main = observer(() => {
+  const addIdea = () => {
     store.ideas.push(new Idea({
-      name: store.name,
+      name: 'test',
       body: store.body,
       quality: store.quality,
     }));
-  }
+  };
 
-  changeBody = (e) => {
-    store.body = e.target.value;
-  }
-
-  changeName = (e) => {
-    store.name = e.target.value;
-  }
-
-  changeQuality= (e) => {
-    store.quality = e.target.value;
-  }
-
-  render() {
-    return (
-      <div>
-        <input placeholder="Idea Name" onChange={this.changeName} />
-        <input placeholder="Idea Body" onChange={this.changeBody} />
-        <select onChange={this.changeQuality} defaultValue="swill">
-          <option value="swill">Swill</option>
-          <option value="plausible">Plausible</option>
-          <option value="genius">Genius</option>
-        </select>
-        <button onClick={this.addIdea}>Submit</button>
-        {store.ideas.map((idea) => {
-          return <p>{idea.title}</p>;
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <input placeholder="Idea Name" onChange={this.changeName} />
+      <input placeholder="Idea Body" onChange={this.changeBody} />
+      <select onChange={this.changeQuality} defaultValue="swill">
+        <option value="swill">Swill</option>
+        <option value="plausible">Plausible</option>
+        <option value="genius">Genius</option>
+      </select>
+      <button onClick={addIdea}>Submit</button>
+      {store.ideas.map((idea) => {
+        return <p>{idea.name}</p>;
+      })}
+    </div>
+  );
+});
 
 export default Main;
+
+// class Main extends Component {
+
+//   addIdea = () => {
+//     store.ideas.push(new Idea({
+//       name: store.name,
+//       body: store.body,
+//       quality: store.quality,
+//     }));
+//   }
+
+//   changeBody = (e) => {
+//     store.body = e.target.value;
+//   }
+
+//   changeName = (e) => {
+//     store.name = e.target.value;
+//   }
+
+//   changeQuality= (e) => {
+//     store.quality = e.target.value;
+//   }
+
+//   render() {
+    // return (
+    //   <div>
+    //     <input placeholder="Idea Name" onChange={this.changeName} />
+    //     <input placeholder="Idea Body" onChange={this.changeBody} />
+    //     <select onChange={this.changeQuality} defaultValue="swill">
+    //       <option value="swill">Swill</option>
+    //       <option value="plausible">Plausible</option>
+    //       <option value="genius">Genius</option>
+    //     </select>
+    //     <button onClick={this.addIdea}>Submit</button>
+    //     {store.ideas.map((idea) => {
+    //       return <p>{idea.title}</p>;
+    //     })}
+    //   </div>
+    // );
+//   }
+// }
+
+// export default Main;
