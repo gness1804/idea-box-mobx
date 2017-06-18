fc26b78cb8f65d46496fd78cc0886463e638cf24
gness1804 <graham.nessler@gmail.com>
2017-06-18 17:41:32 -0500

    Add selenium and passing test for h1

---
+++ test/feature-tests.js
@@ -0,0 +1,27 @@
+/* eslint-disable func-names, prefer-arrow-callback*/
+
+const assert = require('assert');
+const webdriver = require('selenium-webdriver');
+const test = require('selenium-webdriver/testing');
+
+const driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
+
+test.describe('App', function () {
+  this.timeout(15000);
+
+  test.beforeEach(function () {
+    driver.get('http://localhost:3000');
+  });
+
+  test.it('title should render properly', function () {
+    driver.findElement({ tagName: 'h1' }).then(function (title) {
+      return title.getText();
+    }).then(function (text) {
+      assert.strictEqual(text, 'Idea Box');
+    });
+  });
+});
+
+/* eslint-enable func-names, prefer-arrow-callback*/
