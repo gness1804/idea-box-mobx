752e2cb9cd961f91356344e02871182b90f61e20
gness1804 <graham.nessler@gmail.com>
2017-06-18 16:17:05 -0500

    Add passing tests for upvoteProcess

--- test/upvoteProcess.test.js
+++ test/upvoteProcess.test.js
@@ -7,4 +7,10 @@ describe('upvoteProcess', () => {
   it('is a function', () => {
     assert.isFunction(upvoteProcess);
   });
+
+  it('returns the correct value for a quality input', () => {
+    assert.strictEqual(upvoteProcess('Swill'), 'Plausible');
+    assert.strictEqual(upvoteProcess('Plausible'), 'Genius');
+    assert.strictEqual(upvoteProcess('Genius'), 'Genius');
+  });
 });
